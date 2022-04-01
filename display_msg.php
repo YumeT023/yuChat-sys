a53b0d8c5ea07e253ebdf0cdcee1765799cc4f75
Yume <96466759+YumeT023@users.noreply.github.com>
2022-04-02 01:14:32 +0300

    feat: add comments

--- display_msg.php
+++ display_msg.php
@@ -1,11 +1,13 @@
 <?php 
     require __DIR__ . '/vendor/autoload.php';
     
+
     $options = array(
         'cluster' => 'ap2',
         'useTLS' => true
     );
     
+    // Make connection
     $pusher = new Pusher\Pusher(
         'c041ae0a438504593a91',
         '04775ccf064b1c63f178',
@@ -15,18 +17,18 @@
     
     
     
-    // Check the receive message
-    
-    if(isset($_POST['message'])) {
+    // Check the received message
+    if(isset($_POST['message']) AND isset($_POST['name'])) {
 
         $_POST['name'] == 'YumeT023' ? $side = 'R' : $side = '';  
+        // Data will should be structured in specified tag, and removed specialchars such as '<', '<?' to avoid message to execute a script (php, js)
         $data = '<div class="msgComponent ' . $side . '">
                     <header>' . htmlspecialchars($_POST['name']) . '</header>
                     <main>' . htmlspecialchars($_POST['message']) . '</main>
                 </div>';
 
     
-        // Return the received message
+        // trigger the pusher's event & return the structured data
         if($pusher->trigger('yuChat-channel', 'send_message_event', $data)) {
             echo 'success';
         } else {
